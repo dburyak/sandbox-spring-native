09ac748f944f4c76e2d11cc2c69d26eea0c2c3a5
Dmytro Buryak <dmytro.buryak@gmail.com>
2022-07-23 23:53:07 +0300

    Initial setup

---
+++ src/main/java/com/dburyak/sandbox/sandboxspringnative/SandboxSpringNativeApplication.java
@@ -0,0 +1,13 @@
+package com.dburyak.sandbox.sandboxspringnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSpringNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxSpringNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dburyak/sandbox/sandboxspringnative/SandboxSpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dburyak.sandbox.sandboxspringnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxSpringNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
